f5710653b572af90c7165c3ace0f5fd449db2734
Azizbek <webcoderazizbek@gmail.com>
2026-04-21 20:51:51 +0500

    Auto commit: 2026-04-21 20:51:51

--- user-service/internal/domain/cache.go
+++ user-service/internal/domain/cache.go
@@ -1,6 +1,10 @@
+package domain
+
+
+
 type UserCache interface {
 	// Read-through uchun
-	GetUser(ctx context.Context, id uint) (*models.User, error)
+	GetUser(ctx co.Context, id uint) (*models.User, error)
 
 	// Write-through uchun
 	SetUser(ctx context.Context, user *models.User, ttl time.Duration) error
