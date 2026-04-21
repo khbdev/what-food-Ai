16639b3b32d07cceb135877ee2261373d2012237
Azizbek <webcoderazizbek@gmail.com>
2026-04-21 20:56:42 +0500

    Auto commit: 2026-04-21 20:56:42

--- user-service/internal/cache/redis.go
+++ user-service/internal/cache/redis.go
@@ -1,1 +1,27 @@
 package cache
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"user-service/internal/domain"
+	"user-service/internal/models"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type userCache struct {
+	rdb *redis.Client
+}
+
+func NewUserCache(rdb *redis.Client) domain.UserCache {
+	return &userCache{
+		rdb: rdb,
+	}
+}
+
+func key(id uint) string {
+	return fmt.Sprintf("user:%d", id)
+}
