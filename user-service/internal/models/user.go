a09b0d5eed39d9f6b9fe7b4789d00def38742337
Azizbek <webcoderazizbek@gmail.com>
2026-04-21 20:40:41 +0500

    Auto commit: 2026-04-21 20:40:41

--- user-service/internal/models/user.go
+++ user-service/internal/models/user.go
@@ -22,7 +22,7 @@ type User struct {
 	Age     int    `gorm:"not null"`
 	Address string `gorm:"size:255;not null"`
 
-	Email *string `gorm:"size:100;uniqueIndex"`
+	Email string `gorm:"size:100;uniqueIndex"`
 	Image *string `gorm:"size:255"`
 
 	
