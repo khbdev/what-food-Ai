526f5f0b55c4c53755d8e05d7f1110f76a0a0c0f
Azizbek <webcoderazizbek@gmail.com>
2026-04-21 20:11:53 +0500

    Auto commit: 2026-04-21 20:11:53

--- user-service/cmd/main.go
+++ user-service/cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"user-service/internal/config"
 	loadenv "user-service/pkg/loadEnv"
 )
 
@@ -9,7 +10,7 @@ import (
 func main(){
 	loadenv.LoadEnv()
 
-	sql, err := con.NewPostgresDB()
+	sql, err := config.NewPostgresDB()
 	if err != nil{
 	   log.Fatal(err)
 	}
