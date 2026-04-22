bbab74dd0c5813200236719c5737f8b5aeaf11c4
Azizbek <webcoderazizbek@gmail.com>
2026-04-22 18:59:52 +0500

    Auto commit: 2026-04-22 18:59:52

--- user-service/internal/models/request_response.go
+++ user-service/internal/models/request_response.go
@@ -2,19 +2,6 @@ package models
 
 
 
-// CreateUserRequest — yangi user yaratish uchun
-type CreateUserRequest struct {
-	Name  string
-	Phone string
-	Email string
-}
-
-// UpdateUserRequest — user yangilash uchun
-type UpdateUserRequest struct {
-	Name  string
-	Phone string
-	Email string
-}
 
 // GetAllUsersRequest — pagination bilan userlar ro'yxati olish uchun
 type GetAllUsersRequest struct {
