32253df229ec3731098fe289a47cae9acd26264c
Azizbek <webcoderazizbek@gmail.com>
2026-04-22 18:48:38 +0500

    Auto commit: 2026-04-22 18:48:38

--- user-service/internal/handler/user_handler.go
+++ user-service/internal/handler/user_handler.go
@@ -3,7 +3,7 @@ package handler
 import (
 	"context"
 
-	userpb "github.com/khbdev/what-food-proto/proto/user"
+	userpb "github.com/khbdev/what-food-proto/proto/userP"
 
 	"user-service/internal/domain"
 	"user-service/internal/models"
