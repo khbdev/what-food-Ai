bd26a6598da16b76d994177103563d77cfc51500
Azizbek <webcoderazizbek@gmail.com>
2026-04-23 18:31:23 +0500

    Auto commit: 2026-04-23 18:31:23

--- auth-service/pkg/jwt/AccessToken_Generrate.go
+++ auth-service/pkg/jwt/AccessToken_Generrate.go
@@ -1,1 +1,47 @@
 package jwt
+
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"your_project/models"
+)
+
+type Claims struct {
+	UserID   uint   `json:"user_id"`
+	UserName string `json:"user_name"`
+	Role     string `json:"role"`
+	jwt.RegisteredClaims
+}
+
+func GenerateAccessToken(m models.AccessTokenModel) (string, error) {
+	secret := os.Getenv("JWT_ACCESS_SECRET")
+	if secret == "" {
+		return "", ErrMissingSecret
+	}
+
+	expStr := os.Getenv("JWT_ACCESS_EXP_MIN")
+	if expStr == "" {
+		expStr = "15"
+	}
+
+	expMinutes, err := time.ParseDuration(expStr + "m")
+	if err != nil {
+		expMinutes = 15 * time.Minute
+	}
+
+	claims := Claims{
+		UserID:   m.UserID,
+		UserName: m.UserName,
+		Role:     m.Role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(expMinutes)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(secret))
+}
