3d0240860b3eb321b1855dc9292b5ec75ab41898
Azizbek <webcoderazizbek@gmail.com>
2026-04-23 20:34:15 +0500

    Auto commit: 2026-04-23 20:34:15

---
+++ auth-service/pkg/redis/cache.go
@@ -0,0 +1,23 @@
+package redis
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+	"your_project/models"
+)
+
+type Service struct {
+	rdb *redis.Client
+	ctx context.Context
+}
+
+func NewService(rdb *redis.Client) *Service {
+	return &Service{
+		rdb: rdb,
+		ctx: context.Background(),
+	}
+}
