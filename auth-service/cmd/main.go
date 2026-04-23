d20dd81f44211564e5356168b74ba4cb257eb10b
Azizbek <webcoderazizbek@gmail.com>
2026-04-23 18:04:08 +0500

    Auto commit: 2026-04-23 18:04:08

--- auth-service/cmd/main.go
+++ auth-service/cmd/main.go
@@ -1,10 +1,13 @@
 package main
 
-import "auth-service/internal/config"
+import (
+	"auth-service/internal/config"
+	loadenv "auth-service/pkg/LoadEnv"
+)
 
 
 func main(){
-	
+	loadenv.Load()
 	rabbitmq := config.NewRabbit()
 
 	_ = rabbitmq
