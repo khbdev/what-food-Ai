955d4d25435deb923d13d4c3790f0c03bea1dad9
Azizbek <webcoderazizbek@gmail.com>
2026-04-23 18:31:57 +0500

    Auto commit: 2026-04-23 18:31:57

--- auth-service/pkg/jwt/AccessToken_Generrate.go
+++ auth-service/pkg/jwt/AccessToken_Generrate.go
@@ -18,7 +18,7 @@ type Claims struct {
 func GenerateAccessToken(m models.TokenModel) (string, error) {
 	secret := os.Getenv("JWT_ACCESS_SECRET")
 	if secret == "" {
-		return "", ErrMissingSecret
+		return "", 
 	}
 
 	expStr := os.Getenv("JWT_ACCESS_EXP_MIN")
