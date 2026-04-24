38fef14aa331ea02ab5e37e5f3aae281410164d7
Azizbek <webcoderazizbek@gmail.com>
2026-04-24 17:10:23 +0500

    Auto commit: 2026-04-24 17:10:23

--- auth-service/cmd/main.go
+++ auth-service/cmd/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"auth-service/internal/config"
-	"auth-service/internal/models"
+	
 	loadenv "auth-service/pkg/LoadEnv"
 	rabbitMq "auth-service/pkg/rabbitMq"
 	"log"
@@ -27,5 +27,5 @@ func main(){
   producer :=  rabbitMq.NewPublisher(rabbitmq)
 
 
- _ = 
+ _ = producer
 }
