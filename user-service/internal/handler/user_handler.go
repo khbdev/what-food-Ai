111064431bd2892d91c4aca23d5d4fdcb128fede
Azizbek <webcoderazizbek@gmail.com>
2026-04-24 17:30:30 +0500

    Auto commit: 2026-04-24 17:30:30

--- user-service/internal/handler/user_handler.go
+++ user-service/internal/handler/user_handler.go
@@ -5,7 +5,47 @@ import (
 	"user-service/internal/domain"
 	"user-service/internal/models"
 
-	userrpb "github.com/khbdev/what-food-proto/proto/userr"
+	userrpb package jwt
+
+import (
+	"auth-service/internal/models"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type RefreshClaims struct {
+	UserID uint `json:"user_id"`
+	jwt.RegisteredClaims
+}
+
+func GenerateRefreshToken(m models.TokenModel) (string, error) {
+	secret := os.Getenv("JWT_REFRESH_SECRET")
+
+
+	expStr := os.Getenv("JWT_REFRESH_EXP_DAYS")
+	if expStr == "" {
+		expStr = "30"
+	}
+
+	expDays, err := time.ParseDuration(expStr + "24h")
+	if err != nil {
+		expDays = 30 * 24 * time.Hour
+	}
+
+	claims := RefreshClaims{
+		UserID: m.UserID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(expDays)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(secret))
+}
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
