93dcd31578bad53ece527b2d760b233a14527609
Azizbek <webcoderazizbek@gmail.com>
2026-04-24 18:13:56 +0500

    Auto commit: 2026-04-24 18:13:56

--- auth-service/internal/client/user_service.go
+++ auth-service/internal/client/user_service.go
@@ -26,7 +26,7 @@ func NewUserClient(addr string) (*UserClient, error) {
 		return nil, err
 	}
 
-	log.Fatal("user service connection su")
+	fmt("user service connection succesfull")
 	return &UserClient{
 		conn:   conn,
 		client: userrpb.NewUserServiceClient(conn),
