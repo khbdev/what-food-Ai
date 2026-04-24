21f66a017ef22b375bef63a3329bb31de89943ff
Azizbek <webcoderazizbek@gmail.com>
2026-04-24 18:28:20 +0500

    Auto commit: 2026-04-24 18:28:20

--- test/auth/main.go
+++ test/auth/main.go
@@ -58,7 +58,7 @@ func verify(c authpb.AuthServiceClient) {
 	defer cancel()
 
 	res, err := c.VerifyOTP(ctx, &authpb.VerifyRequest{
-		Otp: 123456,
+		Otp: 883868,
 	})
 	if err != nil {
 		log.Fatal(err)
