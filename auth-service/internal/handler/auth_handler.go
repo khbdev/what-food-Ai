e672132b29dbd7dc72c7744dee60557734577f9c
Azizbek <webcoderazizbek@gmail.com>
2026-04-24 18:05:33 +0500

    Auto commit: 2026-04-24 18:05:33

--- auth-service/internal/handler/auth_handler.go
+++ auth-service/internal/handler/auth_handler.go
@@ -1,1 +1,20 @@
 package handler
+
+import (
+	"context"
+
+	"auth-service/internal/domain"
+	"auth-service/internal/models"
+
+	authpb "github.com/khbdev/what-food-proto/proto/authpb"
+)
+
+type AuthHandler struct {
+	authUsecase domain.AuthUsecase
+}
+
+func NewAuthHandler(u domain.AuthUsecase) *AuthHandler {
+	return &AuthHandler{
+		authUsecase: u,
+	}
+}
