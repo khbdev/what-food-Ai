e3657076189ad61b251ece891f9d3fcfd0d54af5
Azizbek <webcoderazizbek@gmail.com>
2026-04-24 17:15:41 +0500

    Auto commit: 2026-04-24 17:15:41

--- auth-service/internal/domain/user_service_cleint.go
+++ auth-service/internal/domain/user_service_cleint.go
@@ -1,1 +1,9 @@
 package domain
+
+
+import userrpb "github.com/khbdev/what-food-proto/proto/userrpb"
+
+type UserService interface {
+	CreateUser(req *userrpb.CreateUserRequest) (*userrpb.UserResponse, error)
+	GetUserByPhone(req *userrpb.GetUserByPhoneRequest) (*userrpb.UserResponse, error)
+}
