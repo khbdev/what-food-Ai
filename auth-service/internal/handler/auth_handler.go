bb6f9ddd8b3cf18b1327d9b85d70d09dbaacb600
Azizbek <webcoderazizbek@gmail.com>
2026-04-24 18:06:20 +0500

    Auto commit: 2026-04-24 18:06:20

--- auth-service/internal/handler/auth_handler.go
+++ auth-service/internal/handler/auth_handler.go
@@ -51,3 +51,17 @@ func (h *AuthHandler) Login(ctx context.Context, req *authpb.LoginRequest) (*aut
 		Message: "success",
 	}, nil
 }
+
+
+func (h *AuthHandler) VerifyOTP(ctx context.Context, req *authpb.VerifyRequest) (*authpb.AuthResponse, error) {
+
+	access, refresh, err := h.authUsecase.Verify(req.Otp)
+	if err != nil {
+		return nil, err
+	}
+
+	return &authpb.AuthResponse{
+		AccessToken:  access,
+		RefreshToken: refresh,
+	}, nil
+}
