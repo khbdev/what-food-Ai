649acf4e27318b76b98dc341b7c6762e088a2e67
Azizbek <webcoderazizbek@gmail.com>
2026-04-24 19:08:14 +0500

    Auto commit: 2026-04-24 19:08:13

--- user-service/internal/domain/usecase.go
+++ user-service/internal/domain/usecase.go
@@ -4,17 +4,11 @@ import (
 	"context"
 	"user-service/internal/models"
 )
-
 type UserUsecase interface {
-	Create(ctx context.Context, req *models.User) error
-
+	Create(ctx context.Context, user *models.User) (*models.User, error) // ← *models.User qaytaradi
 	GetByID(ctx context.Context, id uint) (*models.User, error)
-
 	GetByPhone(ctx context.Context, phone string) (*models.User, error)
-
 	GetAll(ctx context.Context, req *models.GetAllUsersRequest) (*models.GetAllUsersResponse, error)
-
 	Update(ctx context.Context, id uint, req *models.User) error
-
 	Delete(ctx context.Context, id uint) error
 }
