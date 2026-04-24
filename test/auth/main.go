ae8e934167739f85974645d1e5bd5249c2b437c1
Azizbek <webcoderazizbek@gmail.com>
2026-04-24 18:49:57 +0500

    Auto commit: 2026-04-24 18:49:57

--- test/auth/main.go
+++ test/auth/main.go
@@ -28,7 +28,7 @@ func register(c authpb.AuthServiceClient) {
 
 	res, err := c.Register(ctx, &authpb.RegisterRequest{
 		FullName: "Azizbek",
-		Phone:    "+9982324123424198844",
+		Phone:    "+99823241234241988444",
 		Age:      21,
 		Address:  "Tashkent",
 	})
