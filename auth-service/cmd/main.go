b34ad26ead77c3494601e1647ac2666fb38ee4b7
Azizbek <webcoderazizbek@gmail.com>
2026-04-24 19:20:12 +0500

    Auto commit: 2026-04-24 19:20:12

--- auth-service/cmd/main.go
+++ auth-service/cmd/main.go
@@ -35,7 +35,7 @@ func main() {
 	// publisher
 	producer := rabbitMq.NewPublisher(rabbitmq)
 
-	
+	pro
 
 	// user service client (gRPC)
 	userClient, err := client.NewUserClient(os.Getenv("USER_SERVICE"))
