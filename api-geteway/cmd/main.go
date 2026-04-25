e15e8bea4cb5a4aa3d9f7b32314855b5e2c9eeb8
Azizbek <webcoderazizbek@gmail.com>
2026-04-25 15:39:29 +0500

    Auto commit: 2026-04-25 15:39:29

--- api-geteway/cmd/main.go
+++ api-geteway/cmd/main.go
@@ -34,7 +34,7 @@ func main() {
 	// CLIENT (gRPC)
 	// =========================
 	authClient, err := client.NewAuthClient(authURL)
-	userCline
+	userClinet, err := cli
 	if err != nil {
 		log.Fatal("❌ Failed to connect auth service:", err)
 	}
