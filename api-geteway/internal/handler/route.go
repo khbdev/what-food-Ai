1dc06b2b6682243a93648fd7bd8052c504adbf91
Azizbek <webcoderazizbek@gmail.com>
2026-04-25 15:42:20 +0500

    Auto commit: 2026-04-25 15:42:20

--- api-geteway/internal/handler/route.go
+++ api-geteway/internal/handler/route.go
@@ -9,6 +9,7 @@ func SetupRouter(authHandler *AuthHandler, userHandler *UserHandler) *gin.Engine
 
 	r := gin.Default()
 
+	
 	// =========================
 	// AUTH ROUTES
 	// =========================
