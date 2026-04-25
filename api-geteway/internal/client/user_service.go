7a72371af8050480dde9ff3b1162fa2a02fb2a5b
Azizbek <webcoderazizbek@gmail.com>
2026-04-25 11:50:06 +0500

    Auto commit: 2026-04-25 11:50:06

--- api-geteway/internal/client/user_service.go
+++ api-geteway/internal/client/user_service.go
@@ -8,7 +8,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 
-	userrpb "github.com/khbdev/what-food-proto/proto"
+	userrpb "github.com/khbdev/what-food-proto/proto/userr"
 )
 
 type UserClient struct {
