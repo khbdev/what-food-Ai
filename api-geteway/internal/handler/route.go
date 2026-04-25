29681938647096f7195967af5db2e6cb3921d545
Azizbek <webcoderazizbek@gmail.com>
2026-04-25 12:28:40 +0500

    Auto commit: 2026-04-25 12:28:40

--- api-geteway/internal/handler/route.go
+++ api-geteway/internal/handler/route.go
@@ -1,1 +1,24 @@
 package handler
+
+import (
+	"api-geteway/internal/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter(authHandler *AuthHandler) *gin.Engine {
+
+	r := gin.Default()
+
+	// =========================
+	// AUTH ROUTES
+	// =========================
+	auth := r.Group("/auth")
+	{
+		auth.POST("/register", authHandler.Register)
+		auth.POST("/login", authHandler.Login)
+		auth.POST("/verify", authHandler.VerifyOTP)
+	}
+
+	return r
+}
