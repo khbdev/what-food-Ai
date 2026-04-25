ba8ef7eab100d29b2868d084b6f5aa8c23eaad1a
Azizbek <webcoderazizbek@gmail.com>
2026-04-25 10:03:59 +0500

    Auto commit: 2026-04-25 10:03:59

--- test/auth/main.go
+++ test/auth/main.go
@@ -26,9 +26,13 @@ func register(c authpb.AuthServiceClient, id int) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
+	idStr := fmt.Sprintf("%d", id)
+
+	phone := "+99845" + idStr + "434311"
+
 	res, err := c.Register(ctx, &authpb.RegisterRequest{
 		FullName: "Azizbek",
-		Phone:    "+99845434311",
+		Phone:    phone,
 		Age:      21,
 		Address:  "Tashkent",
 	})
