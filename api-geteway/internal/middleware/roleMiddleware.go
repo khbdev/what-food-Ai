91da102fd78a909512a51dd26510528febc2d13e
Azizbek <webcoderazizbek@gmail.com>
2026-04-25 12:03:45 +0500

    Auto commit: 2026-04-25 12:03:45

--- api-geteway/internal/middleware/roleMiddleware.go
+++ api-geteway/internal/middleware/roleMiddleware.go
@@ -1,1 +1,48 @@
 package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtSecret = []byte("secret")
+
+type Claims struct {
+	Role string `json:"role"`
+	jwt.RegisteredClaims
+}
+
+func AdminMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
+
+		token, err := jwt.ParseWithClaims(tokenStr, &Claims{}, func(t *jwt.Token) (interface{}, error) {
+			return jwtSecret, nil
+		})
+
+		if err != nil || !token.Valid {
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		claims := token.Claims.(*Claims)
+
+		// 🔥 ROLE CHECK
+		if claims.Role != "admin" {
+			c.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+
+		c.Next()
+	}
+}
