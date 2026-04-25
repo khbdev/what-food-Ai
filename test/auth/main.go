15669fe97f3d4e4ced653ffc0c4469361c15bad0
Azizbek <webcoderazizbek@gmail.com>
2026-04-25 10:04:05 +0500

    Auto commit: 2026-04-25 10:04:05

--- test/auth/main.go
+++ test/auth/main.go
@@ -26,7 +26,7 @@ func register(c authpb.AuthServiceClient, id int) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	idStr := fmt.Sprintf("%d", id)
+	idStr := ft.Sprintf("%d", id)
 
 	phone := "+99845" + idStr + "434311"
 
