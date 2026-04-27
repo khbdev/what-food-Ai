fcf6456d602d7bf36e40c561b0072776c441600b
Azizbek <webcoderazizbek@gmail.com>
2026-04-27 19:32:00 +0500

    Auto commit: 2026-04-27 19:32:00

--- user-product-service/cmd/main.go
+++ user-product-service/cmd/main.go
@@ -28,5 +28,5 @@ func main(){
 	_ = redis
 
 	repoCategory := repository.NewCategoryRepository(postgress)
-	repoPro := repository.NewCategoryRepository(postgress)
+	repoIncrid := repository.NewCategoryRepository(postgress)
 }
