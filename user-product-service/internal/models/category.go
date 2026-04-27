f6b10f0d28de9a490093c364f6bbcfba85a5f13e
Azizbek <webcoderazizbek@gmail.com>
2026-04-27 18:46:00 +0500

    Auto commit: 2026-04-27 18:46:00

--- user-product-service/internal/models/category.go
+++ user-product-service/internal/models/category.go
@@ -7,3 +7,16 @@ type Category struct {
     Name      string    `db:"name"`
     CreatedAt time.Time `db:"created_at"`
 }
+
+
+type Ingredient struct {
+	ID       int64
+	Name     string
+	Quantity float64
+}
+
+type CategoryWithIngredients struct {
+	CategoryID int64
+	Name       string
+	Items      []Ingredient
+}
