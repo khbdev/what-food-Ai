ced0108683f1781ff7593def9a9523dc0ca5debb
Azizbek <webcoderazizbek@gmail.com>
2026-04-27 18:28:55 +0500

    Auto commit: 2026-04-27 18:28:55

--- user-product-service/internal/config/postgress.go
+++ user-product-service/internal/config/postgress.go
@@ -8,3 +8,41 @@ import (
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
+
+
+func NewPostgresDB() (*sql.DB, error) {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	sslmode := os.Getenv("DB_SSLMODE")
+
+	if host == "" || port == "" || user == "" || dbname == "" {
+		return nil, fmt.Errorf("database env variables not set properly")
+	}
+
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		host, port, user, password, dbname, sslmode,
+	)
+
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// connection test
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	// pool config
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(10)
+	db.SetConnMaxLifetime(time.Hour)
+
+	fmt.Println("PostgreSQL connected (pure sql) 🚀")
+
+	return db, nil
+}
