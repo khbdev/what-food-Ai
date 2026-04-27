99651c9308253bd50a3f203d6acfb6365937d24a
Azizbek <webcoderazizbek@gmail.com>
2026-04-27 20:05:33 +0500

    Auto commit: 2026-04-27 20:05:33

--- user-product-service/cmd/main.go
+++ user-product-service/cmd/main.go
@@ -39,5 +39,11 @@ func main(){
 
 	_ = cacheCategory
 
-	srvCategory := usecase.NewCategoryUsecase(repoCategory, )
+	srvCategory := usecase.NewCategoryUsecase(repoCategory, cacheCategory)
+
+	_ type SortBy []Type
+	
+	func (a SortBy) Len() int           { return len(a) }
+	func (a SortBy) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+	func (a SortBy) Less(i, j int) bool { return a[i] < a[j] }
 }
