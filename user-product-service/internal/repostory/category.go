820ca1a129bc6ca88481cf06c58a21460b740f85
Azizbek <webcoderazizbek@gmail.com>
2026-04-27 18:41:20 +0500

    Auto commit: 2026-04-27 18:41:20

--- user-product-service/internal/repostory/category.go
+++ user-product-service/internal/repostory/category.go
@@ -15,3 +15,13 @@ type categoryRepo struct {
 func NewCategoryRepository(db *sql.DB) domain.CategoryRepository {
 	return &categoryRepo{db: db}
 }
+
+
+func (r *categoryRepo) Create(c *models.Category) error {
+	query := `INSERT INTO categories (name, created_at)
+			  VALUES ($1, NOW())
+			  RETURNING id, created_at`
+
+	return r.db.QueryRow(query, c.Name).
+		Scan(&c.ID, &c.CreatedAt)
+}
