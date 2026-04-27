bd242bc5de4d15292f9322bdcceefb68f8f78994
Azizbek <webcoderazizbek@gmail.com>
2026-04-27 18:29:31 +0500

    Auto commit: 2026-04-27 18:29:31

--- user-product-service/cmd/main.go
+++ user-product-service/cmd/main.go
@@ -1,12 +1,15 @@
 package main
 
-import "user-product-service/internal/config"
+import (
+	"log"
+	"user-product-service/internal/config"
+)
 
 
 
 func main(){
 	postgress, err := config.NewPostgresDB()
 	if err != nil {
-		
+		log.Fatal()
 	}
 }
