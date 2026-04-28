d8d75e320043bd4a885b4b098b834271fe73d345
Azizbek <webcoderazizbek@gmail.com>
2026-04-28 17:10:12 +0500

    Auto commit: 2026-04-28 17:10:12

--- user-product-service/internal/handler/product_handler.go
+++ user-product-service/internal/handler/product_handler.go
@@ -7,7 +7,7 @@ import (
 	"user-product-service/internal/usecase"
 	"user-product-service/internal/models"
 
-	productspb "github.com/khbdev/what-food-proto/proto/incrideats"
+	productspb "github.com/khbdev/what-food-proto/tree/main/proto/incrideats"
 )
 
 type ProductsHandler struct {
