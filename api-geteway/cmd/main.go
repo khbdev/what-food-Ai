cebf16e1d3480ad152e6d6edf7049a18cc8d6607
Azizbek <webcoderazizbek@gmail.com>
2026-04-28 21:13:09 +0500

    Auto commit: 2026-04-28 21:13:09

--- api-geteway/cmd/main.go
+++ api-geteway/cmd/main.go
@@ -74,7 +74,7 @@ userURL := os.Getenv("USER_URL")
 	// =========================
 	authService := service.NewAuthService(authClient)
 	userService := service.NewUserService(userClinet)
-	user
+	userProcuc
 
 	// =========================
 	// HANDLER (HTTP)
