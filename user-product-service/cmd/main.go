b11c1a400ad4c203ac1c019d41d20550638c5cc6
Azizbek <webcoderazizbek@gmail.com>
2026-04-28 17:26:57 +0500

    Auto commit: 2026-04-28 17:26:57

--- user-product-service/cmd/main.go
+++ user-product-service/cmd/main.go
@@ -50,6 +50,5 @@ func main(){
    _ = srcProduct
 
    handCategory := handler.NewCategoryHandler(srvCategory)
-   hand := handler.NewCategoryHandler(srvCategory)
-   hand := handler.NewCategoryHandler(srvCategory)
+   handProduct := handler.NewCategoryHandler(srvCategory)
 }
