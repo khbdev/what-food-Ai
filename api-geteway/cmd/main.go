aa715cf8d909bcb34fc5e64bd552788edc5b43df
Azizbek <webcoderazizbek@gmail.com>
2026-04-28 21:14:21 +0500

    Auto commit: 2026-04-28 21:14:21

--- api-geteway/cmd/main.go
+++ api-geteway/cmd/main.go
@@ -83,7 +83,7 @@ userURL := os.Getenv("USER_URL")
 	// =========================
 	authHandler := handler.NewAuthHandler(authService)
 	userHander := handler.NewUserHandler(userService)
-	userHandler := handler.NewCategoryHandler(us)
+	userHandler := handler.NewCategoryHandler(userCategory)
 
 	// =========================
 	// ROUTER
