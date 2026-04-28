ea809d54369ba2e83076a5b555b0a6bf5471cb64
Azizbek <webcoderazizbek@gmail.com>
2026-04-28 21:11:16 +0500

    Auto commit: 2026-04-28 21:11:16

--- api-geteway/cmd/main.go
+++ api-geteway/cmd/main.go
@@ -26,7 +26,7 @@ userURL := os.Getenv("USER_URL")
 		log.Fatal("❌ AUTH_URL is empty")
 	}
 
-	userProc := os.Getenv("USER_URL")
+	userProductUrl := os.Getenv("USERPR_URL")
 	if authURL == "" {
 		log.Fatal("❌ AUTH_URL is empty")
 	}
