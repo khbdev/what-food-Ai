7604fd166af72c48b23284b20e47d7fae1ce6373
Azizbek <webcoderazizbek@gmail.com>
2026-04-28 20:47:36 +0500

    Auto commit: 2026-04-28 20:47:36

--- api-geteway/internal/client/user_product_service.go
+++ api-geteway/internal/client/user_product_service.go
@@ -9,7 +9,7 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 
 	categorypb "github.com/khbdev/what-food-proto/proto/products"
-	ingredientpb ""
+	ingredientpb "github.com/khbdev/what-food-proto/proto/incrideats"
 )
 
 // =========================
