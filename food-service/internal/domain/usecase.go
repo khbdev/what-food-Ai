cef264ed1a16d57411c5cae3173718733658dbb5
Azizbek <webcoderazizbek@gmail.com>
2026-04-30 19:39:39 +0500

    Auto commit: 2026-04-30 19:39:39

--- food-service/internal/domain/usecase.go
+++ food-service/internal/domain/usecase.go
@@ -14,3 +14,11 @@ type RecipeUsecase interface {
 	Update(ctx context.Context, recipe *models.Recipe) error
 	Delete(ctx context.Context, id int64) error
 }
+
+type SaladUsecase interface {
+	Create(ctx context.Context, salad *models.Salad) error
+	GetByID(ctx context.Context, id int64) (*models.Salad, error)
+	GetAll(ctx context.Context) ([]*models.Salad, error)
+	Update(ctx context.Context, salad *models.Salad) error
+	Delete(ctx context.Context, id int64) error
+}
