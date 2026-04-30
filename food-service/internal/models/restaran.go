c3333ed1cfae89ac4aea7a94a576393614d53af6
Azizbek <webcoderazizbek@gmail.com>
2026-04-30 17:12:31 +0500

    Auto commit: 2026-04-30 17:12:31

--- food-service/internal/models/restaran.go
+++ food-service/internal/models/restaran.go
@@ -1,1 +1,11 @@
 package models
+
+import "time"
+
+type Restaurant struct {
+	ID             int64     `json:"id"`
+	RestaurantName string    `json:"restaurant_name"`
+	Description    string    `json:"description"`
+	ImageURL       string    `json:"image_url"`
+	CreatedAt      time.Time `json:"created_at"`
+}
