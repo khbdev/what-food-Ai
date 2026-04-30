5c0e405481c3f736b9feca003d2c2d7b8ae376c9
Azizbek <webcoderazizbek@gmail.com>
2026-04-30 17:33:04 +0500

    Auto commit: 2026-04-30 17:33:04

--- food-service/internal/domain/repository.go
+++ food-service/internal/domain/repository.go
@@ -13,8 +13,8 @@ type RecipeRepository interface {
 	GetAll(ctx context.Context) ([]*models.Recipe, error)
 
 
-
 	Update(ctx context.Context, recipe *models.Recipe) error
 
 	Delete(ctx context.Context, id int64) error
 }
+
