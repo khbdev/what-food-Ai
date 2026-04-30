61157a3fc6b934fcfba77e3ff4228c5a5fd2afc7
Azizbek <webcoderazizbek@gmail.com>
2026-04-30 19:33:39 +0500

    Auto commit: 2026-04-30 19:33:39

--- food-service/internal/repository/restaran.go
+++ food-service/internal/repository/restaran.go
@@ -1,1 +1,15 @@
 package repository
+
+import (
+	"context"
+	"database/sql"
+	"food-service/internal/models"
+)
+
+type restaurantRepository struct {
+	db *sql.DB
+}
+
+func NewRestaurantRepository(db *sql.DB) RestaurantRepository {
+	return &restaurantRepository{db: db}
+}
