a21cecdf0177c67b6aeb95a900e8b581ea439c3e
Azizbek <webcoderazizbek@gmail.com>
2026-04-30 19:37:44 +0500

    Auto commit: 2026-04-30 19:37:44

--- food-service/internal/usecase/resipe_usecacase.go
+++ food-service/internal/usecase/resipe_usecacase.go
@@ -8,13 +8,7 @@ import (
 	"food-service/internal/models"
 )
 
-type RecipeUsecase interface {
-	Create(ctx context.Context, recipe *models.Recipe) error
-	GetByID(ctx context.Context, id int64) (*models.Recipe, error)
-	GetAll(ctx context.Context) ([]*models.Recipe, error)
-	Update(ctx context.Context, recipe *models.Recipe) error
-	Delete(ctx context.Context, id int64) error
-}
+
 
 type recipeUsecase struct {
 	repo domain.RecipeRepository
