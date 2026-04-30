59de3d3053041f9e23fdfee83e792eb32b4a0506
Azizbek <webcoderazizbek@gmail.com>
2026-04-30 16:45:36 +0500

    Auto commit: 2026-04-30 16:45:36

--- food-service/internal/config/postgres.go
+++ food-service/internal/config/postgres.go
@@ -1,1 +1,48 @@
 package config
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+func NewPostgresDB() (*sql.DB, error) {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	sslmode := os.Getenv("DB_SSLMODE")
+
+	if host == "" || port == "" || user == "" || dbname == "" {
+		return nil, fmt.Errorf("database environment variables not set properly")
+	}
+
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s TimeZone=UTC",
+		host, port, user, password, dbname, sslmode,
+	)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// Ping qilib tekshiramiz
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	// Pool settings
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(10)
+	db.SetConnMaxLifetime(time.Hour)
+
+	log.Println("PostgreSQL connected (database/sql)")
+
+	return db, nil
+}
