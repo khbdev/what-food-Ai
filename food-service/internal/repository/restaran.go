5477418bcdeb1a4a0c0cad49355912917e76221a
Azizbek <webcoderazizbek@gmail.com>
2026-04-30 19:33:58 +0500

    Auto commit: 2026-04-30 19:33:58

--- food-service/internal/repository/restaran.go
+++ food-service/internal/repository/restaran.go
@@ -13,4 +13,28 @@ type restaurantRepository struct {
 
 func NewRestaurantRepository(db *sql.DB) domain.RestaurantRepository {
 	return &restaurantRepository{db: db}
-}RestaurantRepository
+}
+
+func (r *restaurantRepository) Create(ctx context.Context, rest *models.Restaurant) (int64, error) {
+	query := `
+		INSERT INTO restaurants (restaurant_name, description, image_url, created_at)
+		VALUES (?, ?, ?, ?)
+	`
+
+	res, err := r.db.ExecContext(ctx, query,
+		rest.RestaurantName,
+		rest.Description,
+		rest.ImageURL,
+		rest.CreatedAt,
+	)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
