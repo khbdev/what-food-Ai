381524872471e71482af5c92ffcf2f4db3817747
Azizbek <webcoderazizbek@gmail.com>
2026-04-30 16:52:06 +0500

    Auto commit: 2026-04-30 16:52:06

---
+++ food-service/internal/models/food.go
@@ -0,0 +1,24 @@
+package models
+
+import "time"
+
+type Recipe struct {
+	ID          int64     `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+
+	ImageURL string `json:"image_url"`
+	VideoURL string `json:"video_url"`
+
+	Country  string `json:"country"`
+	MealTime string `json:"meal_time"`
+
+	IsSalad bool `json:"is_salad"`
+
+	Kcal    int     `json:"kcal"`
+	Protein float64 `json:"protein"`
+	Fat     float64 `json:"fat"`
+	Carbs   float64 `json:"carbs"`
+
+	CreatedAt time.Time `json:"created_at"`
+}
