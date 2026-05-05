92c549c9cdeb27802d1275576251456f457a9a10
Azizbek <webcoderazizbek@gmail.com>
2026-05-05 16:19:29 +0500

    Auto commit: 2026-05-05 16:19:29

---
+++ ai-service/internal/domain /aimode.go
@@ -0,0 +1,11 @@
+package domai
+
+import (
+	"ai-service/internal/models"
+	"context"
+)
+
+type AIModel interface {
+	AnalyzeMeal(ctx context.Context, req models.MealRequest) (*models.MealResponse, error)
+	AnalyzeNutrition(ctx context.Context, req models.NutritionRequest) (*models.NutritionResponse, error)
+}
