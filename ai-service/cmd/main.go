9ec0962b4557f9947d176423a281a82cb954debf
Azizbek <webcoderazizbek@gmail.com>
2026-05-05 15:55:36 +0500

    Auto commit: 2026-05-05 15:55:36

--- ai-service/cmd/main.go
+++ ai-service/cmd/main.go
@@ -4,6 +4,7 @@ import (
 	aimodel "ai-service/internal/ai-model"
 	"ai-service/internal/models"
 	"ai-service/pkg/env"
+	"context"
 )
 
 
@@ -14,7 +15,7 @@ func main(){
 	
 	_ = groqAi
 
-	ctx, cen
+	ctx, cencel := context.
 
 reqModel := models.MealRequest{
     Name:        "Chicken Burger",
