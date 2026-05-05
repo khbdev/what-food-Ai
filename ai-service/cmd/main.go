7fb59cd487a44df318a5ce9e9d3ae577edc6c519
Azizbek <webcoderazizbek@gmail.com>
2026-05-05 16:18:53 +0500

    Auto commit: 2026-05-05 16:18:53

--- ai-service/cmd/main.go
+++ ai-service/cmd/main.go
@@ -11,3 +11,27 @@ import (
 )
 
 
+func main() {
+	env.LoadEnv()
+
+	groqAi := aimodel.NewGroqClient()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	reqModel := models.NutritionRequest{
+		Period:     "weekly",
+		AvgKcal:    2100,
+		AvgProtein: 65,
+		AvgFat:     80,
+		AvgCarbs:   250,
+	}
+
+	res, err := groqAi.AnalyzeNutrition(ctx, reqModel)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Level:    %s\n", res.Level)
+	fmt.Printf("Feedback: %s\n", res.Feedback)
+}
