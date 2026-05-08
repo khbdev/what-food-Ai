e3a845a214a40b6468c410a061ae51e747a71d66
Azizbek <webcoderazizbek@gmail.com>
2026-05-08 18:00:20 +0500

    Auto commit: 2026-05-08 18:00:20

--- analytics-service/cmd/main.go
+++ analytics-service/cmd/main.go
@@ -29,5 +29,5 @@ func main(){
 
 	useCreate := usecase.NewMealUsecase(repoCreate)
 
-	handCreeate := handler.NewHandlerConsumer(useCreate)
+	handCreeate := handler.NewHandlerConsumer(useCreate, r)
 }
