ac50860a8f76462bd072a3347fa07d385fb607e0
Azizbek <webcoderazizbek@gmail.com>
2026-05-08 17:42:12 +0500

    Auto commit: 2026-05-08 17:42:12

--- analytics-service/internal/config/database.go
+++ analytics-service/internal/config/database.go
@@ -1,1 +1,59 @@
 package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+	"user-service/internal/models"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func NewPostgresDB() (*gorm.DB, error) {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	sslmode := os.Getenv("DB_SSLMODE")
+
+	if host == "" || port == "" || user == "" || dbname == "" {
+		return nil, fmt.Errorf("database environment variables not set properly")
+	}
+
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s TimeZone=UTC",
+		host, port, user, password, dbname, sslmode,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Warn), 
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB.SetMaxOpenConns(25)
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	
+	if err := db.AutoMigrate(
+		&models.User{},
+	); err != nil {
+		return nil, err
+	}
+
+	log.Println("PostgreSQL connected (GORM) & AutoMigrate done")
+
+	return db, nil
+}
