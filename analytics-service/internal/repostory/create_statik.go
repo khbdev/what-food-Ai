d8f248731e03486496b2d41f35af1b24165b4aad
Azizbek <webcoderazizbek@gmail.com>
2026-05-08 17:47:37 +0500

    Auto commit: 2026-05-08 17:47:37

---
+++ analytics-service/internal/repostory/create_statik.go
@@ -0,0 +1,23 @@
+package repository
+
+import (
+	"your_project/models"
+
+	"gorm.io/gorm"
+)
+
+type MealRepository struct {
+	db *gorm.DB
+}
+
+// DI pattern
+func NewMealRepository(db *gorm.DB) *MealRepository {
+	return &MealRepository{
+		db: db,
+	}
+}
+
+// Create meal
+func (r *MealRepository) Create(meal *models.Meal) error {
+	return r.db.Create(meal).Error
+}
