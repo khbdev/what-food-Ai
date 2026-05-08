66c5938d9e71979b874bd7449e3452aec9e5f913
Azizbek <webcoderazizbek@gmail.com>
2026-05-08 17:25:57 +0500

    Auto commit: 2026-05-08 17:25:57

--- analytics-service/cmd/main.go
+++ analytics-service/cmd/main.go
@@ -1,9 +1,13 @@
 package main
 
-import "analytics-service/internal/config"
+import (
+	"analytics-service/internal/config"
+	loadenv "analytics-service/pkg/load_env"
+)
+
 
 func main(){
-	
+	loadenv.LoadEnv()
 	rabbitMq := config.NewRabbit()
 
 	_ = rabbitMq
