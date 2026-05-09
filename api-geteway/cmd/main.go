f4260abd95ed02efaee0591cd93802d985dd3a37
Azizbek <webcoderazizbek@gmail.com>
2026-05-09 15:03:26 +0500

    Auto commit: 2026-05-09 15:03:26

--- api-geteway/cmd/main.go
+++ api-geteway/cmd/main.go
@@ -97,7 +97,7 @@ func main() {
 	productService := service.NewProductService(userProductClient)
 	foodService := service.NewFoodService(foodClient)
 	statikService := service.NewNutritionService(statikClient)
-	mail
+	mailService ;
 
 	// =========================
 	// HANDLERS
