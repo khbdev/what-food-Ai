62d7f5f2ae44869e8e3f5b19669fe31635ec6076
Azizbek <webcoderazizbek@gmail.com>
2026-05-09 10:16:58 +0500

    Auto commit: 2026-05-09 10:16:58

--- api-geteway/internal/client/statik_service.go
+++ api-geteway/internal/client/statik_service.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"time"
 
-	"google.golang.org/grpc"
+	"google.golang.org/grpc"github.com/khbdev/what-food-proto/proto/statik
 	"google.golang.org/grpc/credentials/insecure"
 
 	nutritionpb "github.com/khbdev/what-food-proto/proto/statik"
