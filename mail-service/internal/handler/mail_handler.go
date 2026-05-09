3ab6d1ff0ca9805c6c937bba2bf6173bd51678dc
Azizbek <webcoderazizbek@gmail.com>
2026-05-09 12:02:48 +0500

    Auto commit: 2026-05-09 12:02:48

--- mail-service/internal/handler/mail_handler.go
+++ mail-service/internal/handler/mail_handler.go
@@ -6,7 +6,7 @@ import (
 
 	"mail-service/internal/usecase"
 
-	foodpb "github.com/khbdev/what-food-proto/proto/m"
+	foodpb "github.com/khbdev/what-food-proto/proto/mail"
 )
 
 // =========================
