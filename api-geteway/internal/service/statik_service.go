873f8b749712858d67dc1104195d108f9c800df5
Azizbek <webcoderazizbek@gmail.com>
2026-05-09 10:19:19 +0500

    Auto commit: 2026-05-09 10:19:19

---
+++ api-geteway/internal/service/statik_service.go
@@ -0,0 +1,40 @@
+package service
+
+import (
+	"context"
+	"errors"
+
+	"api-geteway/internal/client"
+
+	nutritionpb "nutrition/proto"
+)
+
+type NutritionService struct {
+	nutritionClient *client.NutritionClient
+}
+
+// =========================
+// INIT
+// =========================
+
+func NewNutritionService(c *client.NutritionClient) *NutritionService {
+	return &NutritionService{
+		nutritionClient: c,
+	}
+}
+
+// =========================
+// GET WEEKLY NUTRITION
+// =========================
+
+func (s *NutritionService) GetWeeklyNutrition(
+	ctx context.Context,
+	req *nutritionpb.WeeklyNutritionRequest,
+) (*nutritionpb.WeeklyNutritionResponse, error) {
+
+	if req.UserId == 0 {
+		return nil, errors.New("user id is required")
+	}
+
+	return s.nutritionClient.GetWeeklyNutrition(req)
+}
