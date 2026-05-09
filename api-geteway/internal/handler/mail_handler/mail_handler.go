9b6fd873b15622924cb4e2039f94d76e3bab5b48
Azizbek <webcoderazizbek@gmail.com>
2026-05-09 14:59:14 +0500

    Auto commit: 2026-05-09 14:59:14

--- api-geteway/internal/handler/mail_handler/mail_handler.go
+++ api-geteway/internal/handler/mail_handler/mail_handler.go
@@ -38,7 +38,7 @@ func (h *FoodHandler) FilterFood(c *gin.Context) {
 
 	hasSalad, _ := strconv.ParseBool(c.DefaultQuery("has_salad", "false"))
 
-	req := &models.FoodFilter{
+	req := &mod.FoodFilter{
 		Country:   c.Query("country"),
 		MealTime:  c.Query("meal_time"),
 		HasSalad:  hasSalad,
