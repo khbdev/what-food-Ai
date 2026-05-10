73d20e93fb9969ab2a0c41ac55f1576e668a061c
Azizbek <webcoderazizbek@gmail.com>
2026-05-10 09:24:16 +0500

    Auto commit: 2026-05-10 09:24:16

--- api-geteway/internal/handler/statik_handler.go
+++ api-geteway/internal/handler/statik_handler.go
@@ -1,3 +1,33 @@
+package handler
+
+import (
+	"net/http"
+
+	"api-geteway/internal/models"
+	"api-geteway/internal/service"
+
+	nutritionpb "github.com/khbdev/what-food-proto/proto/statik"
+	"github.com/gin-gonic/gin"
+)
+
+type NutritionHandler struct {
+	service *service.NutritionService
+}
+
+// =========================
+// INIT
+// =========================
+
+func NewNutritionHandler(s *service.NutritionService) *NutritionHandler {
+	return &NutritionHandler{
+		service: s,
+	}
+}
+
+// =========================
+// GET WEEKLY NUTRITION
+// =========================
+
 func (h *NutritionHandler) GetWeeklyNutrition(c *gin.Context) {
 
 	// =========================
