b0c2f8466de79bb49d24586b01361d859d11abb1
Azizbek <webcoderazizbek@gmail.com>
2026-05-10 13:51:03 +0500

    Auto commit: 2026-05-10 13:51:03

---
+++ auth-service/pkg/jwt/Access_Token_Refresh_Genereate.go
@@ -0,0 +1,66 @@
+package jwt
+
+import (
+	"auth-service/internal/models"
+	"errors"
+	"os"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type RefreshClaims struct {
+	UserID   uint   `json:"user_id"`
+	UserName string `json:"user_name"`
+	Role     string `json:"role"`
+	jwt.RegisteredClaims
+}
+
+func GenerateRefreshToken(refreshToken string) (string, error) {
+
+	// =========================
+	// REFRESH SECRET
+	// =========================
+
+	secret := os.Getenv("JWT_REFRESH_SECRET")
+
+	// =========================
+	// PARSE REFRESH TOKEN
+	// =========================
+
+	token, err := jwt.ParseWithClaims(
+		refreshToken,
+		&RefreshClaims{},
+		func(token *jwt.Token) (interface{}, error) {
+			return []byte(secret), nil
+		},
+	)
+
+	if err != nil {
+		return "", err
+	}
+
+	// =========================
+	// VALIDATE TOKEN
+	// =========================
+
+	claims, ok := token.Claims.(*RefreshClaims)
+	if !ok || !token.Valid {
+		return "", errors.New("invalid refresh token")
+	}
+
+	// =========================
+	// CREATE ACCESS TOKEN
+	// =========================
+
+	accessToken, err := GenerateAccessToken(models.TokenModel{
+		UserID:   claims.UserID,
+		UserName: claims.UserName,
+		Role:     claims.Role,
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	return accessToken, nil
+}
