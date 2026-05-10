af05f8b8b5bbfc8d442f77922d76ffd7d8a89148
Azizbek <webcoderazizbek@gmail.com>
2026-05-10 10:39:53 +0500

    Auto commit: 2026-05-10 10:39:53

--- mail-service/internal/usecase/one_usecase.go
+++ mail-service/internal/usecase/one_usecase.go
@@ -18,7 +18,7 @@ type FoodUsecase struct {
 	foodClient *client.FoodClient
 	aiClient   *client.AiClient
 	rabbit     *config.Rabbit
-	mealCache *rabbitmqproducer,
+	mealCache *ca
 }
 
 func NewFoodUsecase(
