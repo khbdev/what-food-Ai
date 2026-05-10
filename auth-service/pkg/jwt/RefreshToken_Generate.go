24e69ea6e60c6d36fe169ed5de70a28d8c6e98d1
Azizbek <webcoderazizbek@gmail.com>
2026-05-10 13:50:20 +0500

    Auto commit: 2026-05-10 13:50:20

--- auth-service/pkg/jwt/RefreshToken_Generate.go
+++ auth-service/pkg/jwt/RefreshToken_Generate.go
@@ -16,7 +16,6 @@ type RefreshClaims struct {
 func GenerateRefreshToken(m models.TokenModel) (string, error) {
 	secret := os.Getenv("JWT_REFRESH_SECRET")
 
-
 	expStr := os.Getenv("JWT_REFRESH_EXP_DAYS")
 	if expStr == "" {
 		expStr = "30"
