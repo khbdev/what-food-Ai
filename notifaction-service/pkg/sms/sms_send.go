cc17b63e2633d8685ad00584c1ad614207e38843
Azizbek <webcoderazizbek@gmail.com>
2026-05-10 14:51:59 +0500

    Auto commit: 2026-05-10 14:51:59

--- notifaction-service/pkg/sms/sms_send.go
+++ notifaction-service/pkg/sms/sms_send.go
@@ -1,10 +1,37 @@
 package sms
 
-import "log"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"log"
+)
 
 func Send(phone string, otp string) error {
-	log.Println("[SMS MOCK]")
-	log.Println("To:", phone)
-	log.Println("OTP:", otp)
+	log.Println("[TELEGRAM MOCK SMS REPLACEMENT]")
+
+	chatID := os.Getenv("TELEGRAM_CHAT_ID")
+	token := os.Getenv("TELEGRAM_BOT_TOKEN")
+
+	text := fmt.Sprintf("🔐 OTP CODE\nPhone: %s\nOTP: %s", phone, otp)
+
+	url := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", token)
+
+	payload := map[string]any{
+		"chat_id": chatID,
+		"text":    text,
+	}
+
+	body, _ := json.Marshal(payload)
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	log.Println("OTP sent to Telegram group")
 	return nil
 }
