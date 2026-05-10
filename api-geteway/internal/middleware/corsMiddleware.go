356ca26f861f194ca67e568cf68851e8233814df
Azizbek <webcoderazizbek@gmail.com>
2026-05-10 09:29:01 +0500

    Auto commit: 2026-05-10 09:29:01

--- api-geteway/internal/middleware/corsMiddleware.go
+++ api-geteway/internal/middleware/corsMiddleware.go
@@ -6,25 +6,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-
-
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		allowedOrigins := []string{
-			"http://localhost:3000",
-		}
 
 		origin := c.Request.Header.Get("Origin")
-		var allowedOrigin string
-		for _, o := range allowedOrigins {
-			if o == origin {
-				allowedOrigin = o
-				break
-			}
-		}
 
-		if allowedOrigin != "" {
-			c.Writer.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
+		if origin == "http://localhost:3000" {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
 			c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 			c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 			c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
