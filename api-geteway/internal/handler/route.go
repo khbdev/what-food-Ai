d3eb6dc27e6a65fc72875027843e30c7f62a6a5d
Azizbek <webcoderazizbek@gmail.com>
2026-05-12 10:06:52 +0500

    Auto commit: 2026-05-12 10:06:52

--- api-geteway/internal/handler/route.go
+++ api-geteway/internal/handler/route.go
@@ -41,7 +41,7 @@ func SetupRouter(
 		auth.POST("/register", authHandler.Register)
 		auth.POST("/login", authHandler.Login)
 		auth.POST("/verify", authHandler.VerifyOTP)
-		auth.
+		auth.POST("/refresh")
 	}
 
 	// =========================
