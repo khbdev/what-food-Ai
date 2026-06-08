5046c783dcaa813c1485adc54bf6817a62fa83d7
Azizbek <webcoderazizbek@gmail.com>
2026-06-08 19:16:04 +0500

    what-food-ai

--- notifaction-service/internal/models/NotifactionCrud.go
+++ notifaction-service/internal/models/NotifactionCrud.go
@@ -1,10 +1,13 @@
 package models
 
 type Notification struct {
-	ID          string `gorm:"primaryKey;type:char(36)"`
+	ID          uint   `gorm:"primaryKey;autoIncrement"`
 	Title       string `gorm:"type:varchar(255);not null"`
 	Description string `gorm:"type:text;not null"`
 	ImageURL    string `gorm:"type:varchar(500)"`
 	CreatedAt   int64  `gorm:"autoCreateTime"`
 	UpdatedAt   int64  `gorm:"autoUpdateTime"`
 }
+
+//sdfgdpjdfgjdijdsjgjdfiojgsdifjgge
+//rigbnaepiunbeobnenbunbnhgeogoenobjenbjanejbanjenjnjbanjdrbn
