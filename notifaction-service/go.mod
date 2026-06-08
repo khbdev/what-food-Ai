dd703f56fe9c846f6e73b441eee47219184e3398
Azizbek <webcoderazizbek@gmail.com>
2026-06-08 19:50:38 +0500

    what-food-ai

--- notifaction-service/go.mod
+++ notifaction-service/go.mod
@@ -4,9 +4,10 @@ go 1.25.0
 
 require (
 	github.com/joho/godotenv v1.5.1
-	github.com/khbdev/what-food-proto v0.0.3
+	github.com/khbdev/what-food-proto v0.0.4
 	github.com/rabbitmq/amqp091-go v1.11.0
 	github.com/redis/go-redis/v9 v9.20.0
+	google.golang.org/grpc v1.81.1
 	gorm.io/driver/postgres v1.6.0
 	gorm.io/gorm v1.31.1
 )
@@ -26,6 +27,5 @@ require (
 	golang.org/x/sys v0.42.0 // indirect
 	golang.org/x/text v0.35.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20260226221140-a57be14db171 // indirect
-	google.golang.org/grpc v1.81.1 // indirect
 	google.golang.org/protobuf v1.36.11 // indirect
 )
--- notifaction-service/internal/handler/notifaction_gRPC_handler.go
+++ notifaction-service/internal/handler/notifaction_gRPC_handler.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"notifaction-service/internal/models"
 	"notifaction-service/internal/usecase"
+	"strconv"
 
 	pb "github.com/khbdev/what-food-proto/proto/notifaction-crud"
 )
@@ -14,97 +15,83 @@ type NotificationHandler struct {
 }
 
 func NewNotificationHandler(uc *usecase.NotificationUsecase) *NotificationHandler {
-	return &NotificationHandler{
-		uc: uc,
-	}
+	return &NotificationHandler{uc: uc}
 }
 
 func (h *NotificationHandler) CreateNotification(
 	ctx context.Context,
 	req *pb.CreateNotificationRequest,
 ) (*pb.NotificationResponse, error) {
-
 	n := &models.Notification{
 		Title:       req.Title,
 		Description: req.Description,
 		ImageURL:    req.ImageUrl,
 	}
-
 	if err := h.uc.Create(ctx, n); err != nil {
 		return nil, err
 	}
-
 	return &pb.NotificationResponse{
 		Notification: mapToProto(n),
 	}, nil
 }
 
 func (h *NotificationHandler) GetNotification(
-	ctx context.Context,
+	_ context.Context,
 	req *pb.GetNotificationRequest,
 ) (*pb.NotificationResponse, error) {
-
-	n, err := h.uc.GetByID(req.Id)
+	id := strconv.FormatUint(req.Id, 10)
+	n, err := h.uc.GetByID(id)
 	if err != nil {
 		return nil, err
 	}
-
 	return &pb.NotificationResponse{
 		Notification: mapToProto(n),
 	}, nil
 }
 
 func (h *NotificationHandler) GetNotifications(
-	ctx context.Context,
+	_ context.Context,
 	req *pb.GetNotificationsRequest,
 ) (*pb.GetNotificationsResponse, error) {
-
 	list, err := h.uc.GetAll(int(req.Limit), int(req.Offset))
 	if err != nil {
 		return nil, err
 	}
-
 	res := make([]*pb.Notification, 0, len(list))
-
 	for _, n := range list {
 		res = append(res, mapToProto(&n))
 	}
-
 	return &pb.GetNotificationsResponse{
 		Notifications: res,
 	}, nil
 }
 
 func (h *NotificationHandler) UpdateNotification(
-	ctx context.Context,
+	_ context.Context,
 	req *pb.UpdateNotificationRequest,
 ) (*pb.NotificationResponse, error) {
-
 	n := &models.Notification{
-		ID:          req.Id,
+		ID:          uint(req.Id),
 		Title:       req.Title,
 		Description: req.Description,
 		ImageURL:    req.ImageUrl,
 	}
-
 	if err := h.uc.Update(n); err != nil {
 		return nil, err
 	}
-
 	return &pb.NotificationResponse{
 		Notification: mapToProto(n),
 	}, nil
 }
 
 func (h *NotificationHandler) DeleteNotification(
-	ctx context.Context,
+	_ context.Context,
 	req *pb.DeleteNotificationRequest,
 ) (*pb.DeleteNotificationResponse, error) {
-
-	if err := h.uc.Delete(req.Id); err != nil {
+	id := strconv.FormatUint(req.Id, 10)
+	if err := h.uc.Delete(id); err != nil {
 		return nil, err
 	}
-
 	return &pb.DeleteNotificationResponse{
 		Success: true,
 	}, nil
@@ -112,7 +99,7 @@ func (h *NotificationHandler) DeleteNotification(
 
 func mapToProto(n *models.Notification) *pb.Notification {
 	return &pb.Notification{
-		Id:          n.ID,
+		Id:          uint64(n.ID),
 		Title:       n.Title,
 		Description: n.Description,
 		ImageUrl:    n.ImageURL,
